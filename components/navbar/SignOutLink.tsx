95e0a63a8339142304ede044a6ed9198f7efeb4a
Niru1pam2 <nirupamtalukdar123@gmail.com>
2024-07-08 11:38:48 +0530

    add auth provider

--- components/navbar/SignOutLink.tsx
+++ components/navbar/SignOutLink.tsx
@@ -1,7 +1,22 @@
-import React from "react";
+"use client";
+
+import { SignOutButton } from "@clerk/nextjs";
+import { useToast } from "../ui/use-toast";
+import Link from "next/link";
 
 function SignOutLink() {
-  return <div>SignOutLink</div>;
+  const { toast } = useToast();
+  const handleLogout = () => {
+    toast({ description: "Logout Successfull" });
+  };
+
+  return (
+    <SignOutButton>
+      <Link href="/" className="text-left" onClick={handleLogout}>
+        Logout
+      </Link>
+    </SignOutButton>
+  );
 }
 
 export default SignOutLink;
